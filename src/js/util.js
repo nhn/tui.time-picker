8772d04ec0f6b7027bc17f73ce17f829140110fd
김진우 <35218826+jinwoo-kim-nhn@users.noreply.github.com>
2018-04-03 17:56:48 +0900

    feat: add sendhost (#12)
    * feat: add sendhost
    
    * chore: update readme for cdn
    
    * readme update
    
    * apply to codereview
    
    * change readme

--- src/js/util.js
+++ src/js/util.js
@@ -1,10 +1,12 @@
 /**
  * @fileoverview Utils for Datepicker component
  * @author NHN Ent. FE dev Lab. <dl_javascript@nhnent.com>
- * @dependency tui-code-snippet ^1.0.2
+ * @dependency tui-code-snippet ^1.3.0
  */
 
 'use strict';
+
+var snippet = require('tui-code-snippet');
 
 /**
  * Utils
@@ -51,6 +53,22 @@ var utils = {
         }
 
         return arr;
+    },
+
+    /**
+     * send host name
+     * @ignore
+     */
+    sendHostName: function() {
+        var hostname = location.hostname;
+        snippet.imagePing('https://www.google-analytics.com/collect', {
+            v: 1,
+            t: 'event',
+            tid: 'UA-115377265-9',
+            cid: hostname,
+            dp: hostname,
+            dh: 'time-picker'
+        });
     }
 };
 
